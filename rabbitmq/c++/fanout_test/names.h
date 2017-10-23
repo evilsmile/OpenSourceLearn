6cb06ec7605df7e100c6912c2b895b45d4873f5b
evilsmile <freedominmind@163.com>
2017-10-23 20:54:35 +0800

    add fanout_test

---
+++ rabbitmq/c++/fanout_test/names.h
@@ -0,0 +1,12 @@
+#ifndef __NAMES_H__
+#define __NAMES_H__
+
+const std::string LOCAL_EXCHANGE = "fanout_exchange";
+const std::string LOCAL_EXCHANGE_TYPE = "fanout";
+
+const std::string LOCAL_QUEUE_1 = "fanout_queue_for_pic_resize";
+const std::string LOCAL_QUEUE_2 = "fanout_queue_for_pic_cut";
+
+const int LOCAL_CHANNEL_ID = 23;
+
+#endif
