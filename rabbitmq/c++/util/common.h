10e799054fe14e21a12958dadcea39e525cf8601
evilsmile <freedominmind@163.com>
2017-10-24 10:26:49 +0800

    add util/

---
+++ rabbitmq/c++/util/common.h
@@ -0,0 +1,58 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
+#include <amqp_tcp_socket.h>
+#include <amqp.h>
+#include <amqp_framing.h>
+
+
+const std::string HOST_IP = "127.0.0.1";
+//const std::string HOST_IP = "119.23.49.157";
+const int HOST_PORT = 5672;
+const int CHANNEL_ID = 1;
+
+const std::string EXCHANGE_NAME = "flow_ctl_test_exchange";
+const std::string EXCHANGE_TYPE = "direct";
+const std::string ROUTER_NAME = "flow_ctl_test_router";
+
+//const std::string USER = "guest";
+//const std::string PASSWD = "guest";
+const std::string USER = "alert_user";
+const std::string PASSWD = "alertme";
+
+const std::string QUEUE_NAME = "flow_ctl_test_queue";
+
+const int SUMMARY_EVERY_US = 1000000;
+
+const int SEND_MSG_CNT = 1000000;
+
+const int RATE_LIMIT = 5000;
+
+
+////////////////////////////////////////////////////////////////
+//alert_producer/consumer
+const std::string ALERT_EXCHANGE = "alert_exchange";
+const std::string ALERT_EXCHANGE_TYPE = "topic";
+const std::string CRITICAL_QUEUE = "critial_queue";
+const std::string RATING_QUEUE = "rating_queue";
+const std::string CRITICAL_ROUTER = "critical.*";
+const std::string RATING_ROUTER = "*.rate_limit";
+
+
+#define ABORT(msg) \
+    std::cerr << "ABORT: " << msg << std::endl; \
+    exit(-1);
+
+void check_amqp_reply(amqp_connection_state_t& conn, const std::string& show_tip);
+
+void microsleep(int usec);
+
+uint64_t now_us(void);
+
+#endif
