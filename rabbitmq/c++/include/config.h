ae1a6b7f9dd44c0eccafe5406c792a5c5a9e3e74
evilsmile <freedominmind@163.com>
2017-10-24 12:19:46 +0800

    add include/ libs/

---
+++ rabbitmq/c++/include/config.h
@@ -0,0 +1,15 @@
+/* vim:set ft=c ts=2 sw=2 sts=2 et cindent: */
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#ifndef __cplusplus
+# define inline inline
+#endif
+
+/* #undef HAVE_SELECT */
+
+#define HAVE_POLL
+
+#define AMQ_PLATFORM "Linux-3.10.0-514.10.2.el7.x86_64"
+
+#endif /* CONFIG_H */
