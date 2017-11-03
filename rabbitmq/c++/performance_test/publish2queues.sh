8c6567c236c3f08b33919da47eca3af9f22bd7f0
evilsmile <freedominmind@163.com>
2017-11-03 14:01:20 +0800

    add publish2queues.sh

---
+++ rabbitmq/c++/performance_test/publish2queues.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cnt=1000
+if [ $# -gt 0 ]; then
+    cnt=$1
+fi
+
+./producer -p flow_ctl_test_queue -r flow_ctl_test_router -S $cnt -s 10;
+./producer -p flow_ctl_test_queue2 -r flow_ctl_test_router2 -S $cnt -s 10
