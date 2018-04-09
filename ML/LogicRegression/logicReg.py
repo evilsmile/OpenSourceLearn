6d1292b79041dd250ca854e52176dbf491f1696e
evilsmile <freedominmind@163.com>
2018-04-09 16:28:04 +0800

    fix range type problem

--- ML/LogicRegression/logicReg.py
+++ ML/LogicRegression/logicReg.py
@@ -73,7 +73,7 @@ def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     m,n = shape(dataMatrix)
     weights = ones(n)
     for j in range(numIter):
-        dataIndex = range(m)
+        dataIndex = list(range(m))
         for i in range(m): 
             alpha = 4/(1.0+j+i) + 0.01
             randIndex = int(random.uniform(0, len(dataIndex)))
