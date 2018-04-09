79b01cb517fd9f7db9384ec2e4164c3717194867
evilsmile <freedominmind@163.com>
2018-04-09 14:32:47 +0800

    add logicRegression

---
+++ ML/LogicRegression/test.py
@@ -0,0 +1,17 @@
+#!/bin/env python
+#-*- coding:utf8
+
+import logicReg
+from numpy import *
+
+def test():
+    dataMat, labelMat = logicReg.loadDataSet()
+    print "dataMat:",dataMat
+    print "labelMat:",labelMat
+    weights = logicReg.gradAscent(dataMat, labelMat)
+    #weights = logicReg.stocGradAscent0(dataMat, labelMat)
+    weights = logicReg.stocGradAscent2(dataMat, labelMat)
+    print "weights:",weights
+
+if __name__ == '__main__':
+    test()
