d84b86a2436f73b75c7fecda06fea2af7eb9ec0c
evilsmile <freedominmind@163.com>
2018-04-24 15:25:17 +0800

    fix plt problems

--- ML/Regression/test.py
+++ ML/Regression/test.py
@@ -32,16 +32,16 @@ def test2():
     print("lwlr-1.0:",regression.lwlr(xArr[0], xArr, yArr, 1.0))
     print("lwlr-0.001:",regression.lwlr(xArr[0], xArr, yArr, 0.001))
 
-    yHat = regression.lwlrTest(xArr, xArr, yArr, 0.003)
+    yHat = regression.lwlrTest(xArr, xArr, yArr, 0.01)
     print("yHat:", yHat)
     xMat = mat(xArr)
     srtIdx = xMat[:,1].argsort(0)
-    xSort = xmat[srtIdx][:,0,:]
+    xSort = xMat[srtIdx][:,0,:]
 
     import matplotlib.pyplot as plt
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(xSort[:,1], yhat[srtIdx])
+    ax.plot(xSort[:,1], yHat[srtIdx])
     ax.scatter(xMat[:,1].flatten().A[0], mat(yArr).T.flatten().A[0], s=2, c='red')
     plt.show()
 
