eb0f51b275237a01bcf6fae91d346498e340671d
evilsmile <freedominmind@163.com>
2018-04-24 16:01:57 +0800

    add ridge plt

--- ML/Regression/test.py
+++ ML/Regression/test.py
@@ -45,6 +45,44 @@ def test2():
     ax.scatter(xMat[:,1].flatten().A[0], mat(yArr).T.flatten().A[0], s=2, c='red')
     plt.show()
 
+def testAbalone():
+    abX,abY = regression.loadDataSet('abalone.txt')
+
+    # Locally Weighted Linear Regression
+    # smaller kernel will give a lower error, but it will overfit our data
+    yHat01 = regression.lwlrTest(abX[0:99], abX[0:99], abY[0:99], 0.1)
+    yHat1 = regression.lwlrTest(abX[0:99], abX[0:99], abY[0:99], 1)
+    yHat10 = regression.lwlrTest(abX[0:99], abX[0:99], abY[0:99], 10)
+
+    print("rssError-01:", regression.rssError(abY[0:99], yHat01.T))
+    print("rssError-1:", regression.rssError(abY[0:99], yHat1.T))
+    print("rssError-10:", regression.rssError(abY[0:99], yHat10.T))
+
+
+    yHat01 = regression.lwlrTest(abX[100:199], abX[100:199], abY[100:199], 0.1)
+    yHat1 = regression.lwlrTest(abX[100:199], abX[100:199], abY[100:199], 1)
+    yHat10 = regression.lwlrTest(abX[100:199], abX[100:199], abY[100:199], 10)
+
+    print("rssError-01:", regression.rssError(abY[100:199], yHat01.T))
+    print("rssError-1:", regression.rssError(abY[100:199], yHat1.T))
+    print("rssError-10:", regression.rssError(abY[100:199], yHat10.T))
+
+    # Simple Linear regression
+    ws = regression.standRegres(abX[0:99], abY[0:99])
+    yHat = mat(abX[100:199])*ws
+    print("rssError-Simp:", regression.rssError(abY[100:199], yHat.T.A))
+
+def testRidge():
+    abX,abY = regression.loadDataSet('abalone.txt')
+    ridgeWeights = regression.ridgeTest(abX, abY)
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plt(ridgeWeights)
+    plt.show()
+
 if __name__ == '__main__':
     #test()
-    test2()
+    #test2()
+    #testAbalone()
+    testRidge()
