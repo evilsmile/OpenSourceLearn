ef9e8d16f5837b8c8e4426c2da94d20c6ad1b0cb
evilsmile <freedominmind@163.com>
2018-04-24 14:36:35 +0800

    add Regression/

---
+++ ML/Regression/test.py
@@ -0,0 +1,29 @@
+#!/bin/env python
+#-*- coding:utf8
+
+import regression
+from numpy import *
+
+def test():
+    xArr, yArr = regression.loadDataSet('ex0.txt')
+    print("xArr:", xArr, " yArr:", yArr)
+
+    ws = regression.standRegres(xArr, yArr)
+    print("ws:", ws)
+
+    xMat = mat(xArr)
+    yMat = mat(yArr)
+    yHat = xMat * ws
+
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xMat[:,1].flattern().A[0], yMat.T[:,0].flattern().A[0])
+
+    xCopy = xMat.copy()
+    xCopy.sort(0)
+    yHat = xCopy(:,1], yHat)
+    plt.show()
+   
+if __name__ == '__main__':
+    test()
