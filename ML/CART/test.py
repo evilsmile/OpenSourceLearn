1f8561637cf1ff9640f6f18d3ee30ec1c61fcfb2
evilsmile <freedominmind@163.com>
2018-04-26 14:48:34 +0800

    add ex2.txt test

--- ML/CART/test.py
+++ ML/CART/test.py
@@ -16,9 +16,17 @@ def test():
 
     myDat1 = cart.loadDataSet("ex0.txt")
     myMat1 = mat(myDat1)
-    print("createTree2:", cart.createTree(myMat1))
+    print("createTree1:", cart.createTree(myMat1))
     plt.plot(myMat1[:,0], myMat1[:,1], 'ro')
     plt.show()
+
+    myDat2 = cart.loadDataSet("ex2.txt")
+    myMat2 = mat(myDat2)
+    print("createTree2:", cart.createTree(myMat2))
+    plt.plot(myMat2[:,0], myMat2[:,1], 'ro')
+    plt.show()
+
+        
  
 if __name__ == '__main__':
     test()
