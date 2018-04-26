6645a8346bd61d0b61a928875ea059a511fb8f6e
evilsmile <freedominmind@163.com>
2018-04-26 14:42:46 +0800

    add ex0.txt plt show

--- ML/CART/test.py
+++ ML/CART/test.py
@@ -13,6 +13,12 @@ def test():
 
     plt.plot(myMat[:,0], myMat[:,1], 'ro')
     plt.show()
-    
+
+    myDat1 = cart.loadDataSet("ex0.txt")
+    myMat1 = mat(myDat1)
+    print("createTree2:", cart.createTree(myMat1))
+    plt.plot(myMat1[:,0], myMat1[:,1], 'ro')
+    plt.show()
+ 
 if __name__ == '__main__':
     test()
