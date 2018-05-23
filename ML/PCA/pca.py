965f2feb7b50c7f37bbf58e9ccccdacd8092ef03
evilsmile <freedominmind@163.com>
2018-05-23 11:28:47 +0800

    add PCA comment

--- ML/PCA/pca.py
+++ ML/PCA/pca.py
@@ -13,13 +13,17 @@ def pca(dataMat, topNfeat=999999):
     # calculate the mean of the original dataset
     meanVals = mean(dataMat, axis=0)
     # remove mean
+    # 去除平均值 
     meanRemoved = dataMat - meanVals
     # compute the covariance(协方差) matrix 
+    # 计算协方差矩阵
     covMat = cov(meanRemoved, rowvar=0)
     # calculate the eigenvalues(特征值)
+    # 计算协方差矩阵的特征值和特征向量
     eigVals,eigVects = linalg.eig(mat(covMat))
     # sort top N smallest to largest
     # get the order of the eigenvalues
+    # 将特征值从大到小排序，保留对应的最上面的N个特征向量
     eigValInd = argsort(eigVals)
     eigValInd = eigValInd[:-(topNfeat+1):-1]
     # use the order of the eigenvalues to sort the eigenvectors
@@ -28,6 +32,15 @@ def pca(dataMat, topNfeat=999999):
     redEigVects = eigVects[:,eigValInd]
     lowDDataMat = meanRemoved * redEigVects
     # Transform data into new dimensions, the reduced dimension dataset.
+    # 将数据转换到N个特征向量构建的新空间中
     reconMat = (lowDDataMat * redEigVects.T) + meanVals
 
     return lowDDataMat, reconMat
+
+def replaceNanWithMean(fileName, delim='\t'):
+    datMat = loadDataSet(fileName, delim)
+    numFeat = shape(datMat)[1]
+    for i in range(numFeat):
+        meanVal = mean(datMat[nonzero(~isnan(datMat[:,i].A))[0], i])
+        datMat[nonzero(isnan(datMat[:,i].A))[0],i] = meanVal
+    return datMat
--- ML/PCA/test.py
+++ ML/PCA/test.py
@@ -19,5 +19,22 @@ def test():
     ax.scatter(reconMat[:,0].flatten().A[0], reconMat[:,1].flatten().A[0], marker='o', s=50, c='red')
     plt.show()
 
+
+def test2():
+    # secom.data 来自UCI机器学习数据库，包含590个特征， 其中几乎所有样本都存在特征缺失，
+    # 用NaN表示，通过replaceNanWithMean将缺失的NaN数据用其它样本的相同特征值平均值填充
+    dataMat = pca.replaceNanWithMean('secom.data', ' ') 
+    print "dataMat: ", dataMat
+    meanVals = mean(dataMat, axis=0)
+    meanRemoved = dataMat - meanVals
+    covMat = cov(meanRemoved, rowvar=0)
+    print "covMat:", covMat
+    eigVals,eigVects = linalg.eig(mat(covMat))
+    # 有超过20%的特征值都为0，这些特征都是其他特征的副本
+    # 前6个特征覆盖了96.8%的方差
+    print "eigVals: ", eigVals
+    print "eigVects: ", eigVects
+
 if __name__ == '__main__':
-    test()
+    #test()
+    test2()
